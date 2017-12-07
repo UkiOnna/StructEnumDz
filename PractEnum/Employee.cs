545dca5270d2d2444edd440f7b7318c0b989f6f1
UkiOnna <sikirovtemirlan@mail.ru>
2017-12-07 16:18:22 +0600

    hohoho

---
+++ PractEnum/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractEnum
+{
+    public struct Employee
+    {
+        private string _name;
+        private Vacancies _vacancy;
+        private int _salary;
+        private int[] _dataEnter;
+
+        public Employee (string name,Vacancies vacancy,int salary,int[] dataEnter)
+        {
+            _name = name;
+            _vacancy = vacancy;
+            _salary = salary;
+            _dataEnter = new int[3];
+            for(int i = 0; i < _dataEnter.Length; i++)
+            {
+                _dataEnter[i] = dataEnter[i];
+            }
+        }
+
+       
+    }
+}
---
+++ PractEnum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lexa";
+           
+            int salary = 5;
+            int[] dataEntered = new int[3];
+            for(int i = 0; i < dataEntered.Length; i++)
+            {
+                dataEntered[i] = i + 10;
+            }
+            Employee employee=new Employee (name,Vacancies.Boss,salary,dataEntered);
+            Console.WriteLine("Имя - {0}, Должность - {1}, Зарплата - {2}, Дата Создания {3}.{4}.{5}",name,Vacancies.Boss,salary,dataEntered[0],dataEntered[1],dataEntered[2]);
+            Console.ReadLine();
+        }
+    }
+}
